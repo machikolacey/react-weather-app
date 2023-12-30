4202bb006cb4858f0afb2d6c628608055c9e00c9
Kim Kimura <kim.kimura@storm12.co.uk>
2023-12-30 03:39:24 +0000

    added datatype

--- apps/weather-app/pages/index.tsx
+++ apps/weather-app/pages/index.tsx
@@ -91,7 +91,7 @@ function Index() {
 	  }
 	};
 
-	const formatAPIResponseDate = (dateString) => {
+	const formatAPIResponseDate = (dateString: string) => {
 	  const [year, month, day] = dateString.split("-"); 
 	  return `${day}/${month}/${year}`;
 	};
